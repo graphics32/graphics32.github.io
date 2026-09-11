9b6fb54a6b0cbc7a0579fdeffba84419a0a139c8
andersmelander <andersmelander@users.noreply.github.com>
2026-09-11 07:37:26 +0000

    Automated staging build from graphics32 main repository graphics32/graphics32@e2d1bdf59d42ca3d1c0d72adb3a6b425e6d9ee8b

---
+++ assets/chunks/swimlanesDiagram-VK2B7HYN.E16pQcLt.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-A5DVABFB.D3SLbqWT.js";import{_ as a}from"../app.oFQfOQ_v.js";import"./chunk-5VM5RSS4.Bkw2kzks.js";import"./chunk-XXDRQBXY.CC9YmbSE.js";import"./chunk-POPQ4Y6H.W7nNx9Io.js";import"./chunk-F27PBJKO.DbXo8ajw.js";import"./channel.B9MOPEni.js";import"./framework.BXWQBDlf.js";import"./theme.B5msR_rr.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
