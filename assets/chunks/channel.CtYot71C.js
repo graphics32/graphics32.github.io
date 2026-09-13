cacfad83834dc7c25c176d8e6a25f7bf2ced4d27
andersmelander <andersmelander@users.noreply.github.com>
2026-09-13 22:49:29 +0000

    Automated staging build from graphics32 main repository graphics32/graphics32@b6a9a740a34e189c5a42e7e222777163d36ee793

---
+++ assets/chunks/channel.CtYot71C.js
@@ -0,0 +1,1 @@
+import{U as a,C as n}from"../app.DvEdmjS7.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ assets/chunks/swimlanesDiagram-VK2B7HYN.DSG0wB52.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-A5DVABFB.C0cIOZVb.js";import{_ as a}from"../app.DvEdmjS7.js";import"./chunk-5VM5RSS4.n6Uuwz8U.js";import"./chunk-XXDRQBXY.CaQYwGch.js";import"./chunk-POPQ4Y6H.CwG_L1or.js";import"./chunk-F27PBJKO.BPm97shG.js";import"./channel.CtYot71C.js";import"./framework.BXWQBDlf.js";import"./theme.U3pxPRiZ.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
